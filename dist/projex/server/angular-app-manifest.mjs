a68696dd36ac4a611c6a0622131ce094146e42ad
ProjexERP <webmaster@tendernews.com>
2025-06-14 15:13:46 +0530

    first commit

--- dist/projex/server/angular-app-manifest.mjs
+++ dist/projex/server/angular-app-manifest.mjs
@@ -40,21 +40,31 @@ export default {
   {
     "renderMode": 2,
     "route": "/projex/contactUs"
+  },
+  {
+    "renderMode": 2,
+    "route": "/projex/login"
+  },
+  {
+    "renderMode": 2,
+    "route": "/projex/erp/selectform-new"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 12567, hash: '82fa01a659cd2b8e340b790d1462e473184fc28791431a940386bc078904a9f0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1139, hash: 'b18b87d971326cdf4904001d212b0af1194a4c1f2f55e43676b37a5744d1ff64', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'payroll/index/index.html': {size: 189300, hash: '5ce767507b19380bb9de7ac3157681798dda3c00a00558cb56e4050bdbf1622d', text: () => import('./assets-chunks/payroll_index_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 171262, hash: 'abfdc1ec2bcd3b29ddc49a2b556b70e0f75fdb0512269c67d13e9ba61ff936de', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'product/prodandplan/index.html': {size: 114079, hash: '58d57d203bc6d0aaf8c8f7555ffca1e25ca5d65e3671903efc02ad2a5322b21f', text: () => import('./assets-chunks/product_prodandplan_index_html.mjs').then(m => m.default)},
-    'manage-account/index.html': {size: 95981, hash: '605d95c2650120e8b331a85b16e2bdc127d4cbf8522cac656e4113f1e1c034c8', text: () => import('./assets-chunks/manage-account_index_html.mjs').then(m => m.default)},
-    'product/Planandpricing/index.html': {size: 174725, hash: 'c4c96f9e793b973ff767fde9ffd761b382acaa10724cff0b5a25638b84e0b35a', text: () => import('./assets-chunks/product_Planandpricing_index_html.mjs').then(m => m.default)},
-    'hrm/index.html': {size: 96145, hash: '46b4b294f563901e5bb9bc10a5eaf905cf76d004bd46e48bd7e38ee7f512952b', text: () => import('./assets-chunks/hrm_index_html.mjs').then(m => m.default)},
-    'sale-order-management/index.html': {size: 85848, hash: '1b74499a3eeb155055e9cb9637e8e97dfa9c670ef6db4b72c47c5d04e78512d2', text: () => import('./assets-chunks/sale-order-management_index_html.mjs').then(m => m.default)},
-    'inventory-control-management/index.html': {size: 84882, hash: '2c341f29f68883283adc7d5adc910873ac203bf600a71f21a891baa0ab4e6ad1', text: () => import('./assets-chunks/inventory-control-management_index_html.mjs').then(m => m.default)},
-    'contactUs/index.html': {size: 85251, hash: '04e6b81259b2abe987ed0807606841c1a7059c4e476f1096ddc872ed92623ceb', text: () => import('./assets-chunks/contactUs_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 12652, hash: '700c3964ae41c457755cf12d3cf89e84654fdd394ddc5500059d29aecba923cf', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1224, hash: '8e2a1323ee100057ee25d9eba9c12f36978a7537d1090441b3b29ce5fe2fa8e6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'payroll/index/index.html': {size: 189379, hash: '16678536355ccc5df3b1da2e4e2f871ba0fd6ad39bf0a598c35d75e7761afeea', text: () => import('./assets-chunks/payroll_index_index_html.mjs').then(m => m.default)},
+    'product/Planandpricing/index.html': {size: 174804, hash: '6483d11f153929c98de97bfadb0f69166c92db7be8b4cdd4417e3880c682e5a8', text: () => import('./assets-chunks/product_Planandpricing_index_html.mjs').then(m => m.default)},
+    'manage-account/index.html': {size: 96060, hash: 'f98c7ce27ea397012c5d67ed11eca8d40ab8072d0f30a2aa7d6ad835e4e4f3eb', text: () => import('./assets-chunks/manage-account_index_html.mjs').then(m => m.default)},
+    'product/prodandplan/index.html': {size: 114158, hash: 'dc2bfb334c2586a466f3f90d59a0feff04b4dece15b663be085d02421f6c08ac', text: () => import('./assets-chunks/product_prodandplan_index_html.mjs').then(m => m.default)},
+    'hrm/index.html': {size: 96224, hash: 'd617508363343d2b3c11a86785429fe0f6fcbbe7a0616363bbb979be50a1b8d4', text: () => import('./assets-chunks/hrm_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 171341, hash: '457af488fdbc80c0f784c304095fe696f9f6e0149cb4b1a2db51f2865c6bacae', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'sale-order-management/index.html': {size: 85927, hash: 'd8f56af17081b26c654e8713262d6102e9d09e76df9d2a3896ec5d34f062b905', text: () => import('./assets-chunks/sale-order-management_index_html.mjs').then(m => m.default)},
+    'inventory-control-management/index.html': {size: 84961, hash: 'e0f70c274fefcc36bc4ed2a99d3a994bb2abc1d26b587020800977d27311669c', text: () => import('./assets-chunks/inventory-control-management_index_html.mjs').then(m => m.default)},
+    'contactUs/index.html': {size: 85330, hash: '80209815beed9448e7ebc66731b8937a034267b551cfe2c955cf09185c446604', text: () => import('./assets-chunks/contactUs_index_html.mjs').then(m => m.default)},
+    'erp/selectform-new/index.html': {size: 74561, hash: '103f38ef18266a861dc7bac91b82708da482229e19e77e4b9782efe5021b813a', text: () => import('./assets-chunks/erp_selectform-new_index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 81940, hash: '189c29de447d98137645a06806bdcf60df64a031fbcda9ef0324b19c7a87fbd1', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
     'styles-4E3EC4UF.css': {size: 181255, hash: 'DSJ80i83ygE', text: () => import('./assets-chunks/styles-4E3EC4UF_css.mjs').then(m => m.default)}
   },
 };
